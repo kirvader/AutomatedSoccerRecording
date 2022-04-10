f914207c7fe928578bb52fe876cd306d7829bd61
kirvader <kirkondratiuk@gmail.com>
2022-04-10 19:05:15 +0300

    added some scripts

---
+++ temp_utils/model_processor.py
@@ -0,0 +1,23 @@
+from yolov5.detect import run
+
+
+def process_source_with_model(
+    model='yolov5x.pt',  # the biggest one is by default
+    source='testing_samples/images/',
+    project_path='testing_samples',
+    img_sz=(1280, 1280),
+    name='.'
+):
+    run(weights=model,
+        source=source,
+        save_txt=True,
+        imgsz=img_sz,
+        max_det=30,
+        nosave=True,
+        project=project_path,
+        name=name,
+        classes=[32],
+        device=0
+        )
+
+# process_source_with_model(source='VideoSlicer/output/frames/frame103.jpg')
